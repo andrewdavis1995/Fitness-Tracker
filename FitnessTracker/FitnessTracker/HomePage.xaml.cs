fc5edf064413d23783df09e24012a100dced75bd
andrewdavis1995 <asutherlandavis@googlemail.com>
2022-10-21 17:27:08 +0100

    Project setup

---
+++ FitnessTracker/FitnessTracker/HomePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace FitnessTracker
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class HomePage : Window
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
