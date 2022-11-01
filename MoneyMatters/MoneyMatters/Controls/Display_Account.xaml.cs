d524d9cd220ae16228ba58a4dd4b1888b94be54c
Andrew Davis <asutherlandavis@googlemail.com>
2022-11-01 13:04:12 +0000

    Update Display_Account.xaml.cs

--- MoneyMatters/MoneyMatters/Controls/Display_Account.xaml.cs
+++ MoneyMatters/MoneyMatters/Controls/Display_Account.xaml.cs
@@ -13,10 +13,17 @@ namespace MoneyMatters.Controls
     /// </summary>
     public partial class Display_Account : UserControl
     {
+        BankAccount _account;
+    
+        /// <summary>
+        /// Constructor
+        /// </summary>
         public Display_Account(BankAccount account)
         {
             InitializeComponent();
+            _account = account;
 
+            // setup display with correct data
             txtAccountName.Text = account.GetAccountName();
             txtBalance.Text = "£" + account.GetBalance().ToString("0.00");
             txtBankName.Text = account.GetBankName();
@@ -25,7 +32,6 @@ namespace MoneyMatters.Controls
 
             try
             {
-
                 imgIcon.Source = new BitmapImage(new Uri("pack://application:,,,/"
                     + Assembly.GetExecutingAssembly().FullName +
                     ";component/Images/Icons/" + account.GetBankName() + ".png"));
@@ -34,14 +40,30 @@ namespace MoneyMatters.Controls
             { }
         }
 
+        /// <summary>
+        /// Event handler for when the mouse enters this control
+        /// </summary>
         private void UserControl_MouseEnter(object sender, System.Windows.Input.MouseEventArgs e)
         {
             Highlight.Visibility = System.Windows.Visibility.Visible;
         }
 
+        /// <summary>
+        /// Event handler for when the mouse leaves this control
+        /// </summary>
         private void UserControl_MouseLeave(object sender, System.Windows.Input.MouseEventArgs e)
         {
             Highlight.Visibility = System.Windows.Visibility.Collapsed;
+        }
+        
+        
+        /// <summary>
+        /// Accessor for the account linked to this display
+        /// </summary>
+        /// <returns>The linked account</returns>
+        public BankAccount GetAccount()
+        {
+            return _account;
         }
     }
 }
